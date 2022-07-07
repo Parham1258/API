6c30ae530f6fcd3770879b61c452bd6709a3a80f
Parham <95162943+Parham1258@users.noreply.github.com>
2022-07-07 11:50:32 +0430

    Vercel

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 #API
+from crypt import methods
 import os
 from flask import Flask, render_template, send_file, request
 import random
@@ -53,7 +54,7 @@ def Handler_404(error):
 def Home(): return render_template("Home.html")
 @app.route("/API")
 def API(): return render_template("API.html")
-@app.route("/API/Password-Generator")
+@app.route("/API/Password-Generator", methods=["GET", "POST"])
 def API_Password_Generator():
     characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     generated_password = ""
