7a0b18fe1301e326af94b1ad0813d49413126383
Parham <95162943+Parham1258@users.noreply.github.com>
2022-07-21 18:15:38 +0430

    Some Change

--- Parham API.py
+++ Parham API.py
@@ -63,29 +63,29 @@ def API(): return render_template("API.html"), 200
 @app.route("/API/Password-Generator", methods=["GET", "POST"])
 def API_Password_Generator():
     characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    generated_password = ""
+    Generated_Password = ""
     if "Characters" in request.args:
         try:
             length = int(request.args["Characters"])
             if length > 100: length = 20
         except: length = 20
     else: length = 20
-    for i in range(length): generated_password += random.choice(characters)
-    return "{ \"Generated Password\": \""+generated_password+"\" }", 200
+    for i in range(length): Generated_Password += random.choice(characters)
+    return {"Generated Password": Generated_Password}, 200
 @app.route("/API/8ball", methods=["GET", "POST"])
 def API_8ball():
     words = ["Yes", "no", "maybe", "not now", "never", "100%", "nope", "idk", "Yeah", "no u", "ye", "No"]
     word = random.choice(words)
-    return "{ \"8ball\": \""+word+"\" }", 200
+    return {"8ball": word}, 200
 @app.route("/API/UNIX", methods=["GET", "POST"])
-def API_UNIX(): return "{ \"UNIX\": \""+str(time.time())+"\" }", 200
+def API_UNIX(): return {"UNIX": str(time.time())}, 200
 @app.route("/API/Reverse-Text")
 def API_Reverse_Text():
     if "Text" in request.args and request.args["Text"] != "": Text = request.args["Text"]
     else: Text = "Hello"
     Text.replace("\"", "\\\"")
     Reversed_Text = Text[::-1]
-    return "{ \"Reversed Text\": \""+Reversed_Text+"\" }", 200
+    return {"Reversed Text": Reversed_Text}, 200
 @app.route("/API/Random-Number", methods=["GET", "POST"])
 def API_Random_Number():
     if "From" in request.args and "To" in request.args:
@@ -102,7 +102,7 @@ def API_Random_Number():
         From = 1
         To = 10
     number = random.randint(From, To)
-    return "{ \"Random Number\": \""+str(number)+"\" }", 200
+    return {"Random Number": str(number)}, 200
 @app.route("/API/Random-Floating-Number", methods=["GET", "POST"])
 def API_Floating_Random_Number():
     if "From" in request.args and "To" in request.args:
@@ -119,10 +119,10 @@ def API_Floating_Random_Number():
         From = 1
         To = 10
     number = random.uniform(From, To)
-    return "{ \"Random Floating Number\": \""+str(number)+"\" }", 200
+    return {"Random Floating Number": str(number)}, 200
 @app.route("/Assets/<string:File>")
 def Assets(File):
-  if os.path.exists("Assets/"+File): return send_file("Assets/"+File), 200
+  if os.path.exists("Assets\\"+File): return send_file("Assets/"+File), 200
   else: return "File Doesn't Exists", 404
 @app.route("/favicon.ico")
 def Icon(): return redirect("/Assets/API.png"), 301
